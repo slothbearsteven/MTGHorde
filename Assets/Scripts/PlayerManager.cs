a4ebaf4b7e138d2015b4368d142a60fbb8715380
Steven Kennedy <53795520+slothbearsteven@users.noreply.github.com>
2022-04-28 11:44:05 -0600

    Set up player script to know what to code l8r

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using unityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public static bool isHordeMode;
+
+    private int life = 20;
+    private int playerCounters;
+    public Text lifeText;
+
+    public Text counterText;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void MinusLife() { }
+
+    void AddLife() { }
+
+    void AddCounters() { }
+
+    void MinusCounters() { }
 }
